8edddc7188a8e6492eafc5dcacb474b740bd49cb
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2023-09-01 19:45:08 -0400

    first commit

---
+++ app/exemplo/views.py
@@ -0,0 +1,17 @@
+import logging
+
+from django.shortcuts import render
+
+from .models import Question
+
+logger = logging.getLogger('mysite')
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    logger.warning(
+        'This is a warning!',
+        exc_info=True
+    )
+    return render(request, 'exemplo/index.html', context)
